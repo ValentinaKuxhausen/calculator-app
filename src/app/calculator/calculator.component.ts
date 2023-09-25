b0411fe7777a6b45f0e22d52268f087aa20207f0
ValentinaKuxhausen <valentina.kuxhausen@web.de>
2023-09-25 15:04:33 +0200

    shorten code

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -6,9 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./calculator.component.sass'],
 })
 export class CalculatorComponent {
-  result: string = '';
+  result: string = '0';
+  numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+  clickChange() {
+    //remove loading "0" when user clicks on inputfield
+    this.result = '';
+  }
 
   appendToResult(value: string) {
+    //remove loading "0" when user clicks on number or comma button
+    if (this.result === '0') {
+      this.result = '';
+    }
     this.result += value;
   }
 
@@ -16,11 +26,11 @@ export class CalculatorComponent {
     try {
       this.result = eval(this.result);
     } catch (error) {
-      this.result = 'Error';
+      this.result = 'error';
     }
   }
 
   clear() {
-    this.result = '';
+    this.result = '0';
   }
 }
